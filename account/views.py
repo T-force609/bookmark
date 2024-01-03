a0e2190966a765df7dc342807845987d4bd5f317
T-force609 <johnolusegun21@gmail.com>
2024-01-03 09:43:19 +0100

    register form was added to the website

--- account/views.py
+++ account/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm,\
+                    UserEditForm, ProfileEditForm
+from .models import Profile
 # Create your views here.
 
 def user_login(request):
@@ -29,3 +31,33 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section':'dashboard'})
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password1'])
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            return render(request, 'account/register_done.html', {'new_user':new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form':user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+        
+    return render(request, 'account/edit.html', {'user_form':user_form, 'profile_form':profile_form})
+    
